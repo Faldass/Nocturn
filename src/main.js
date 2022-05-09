1c13114e22a2e7acc993a6b1c1036db7baad1fe2
Faldass <quentin.terzi@otlook.fr>
2022-05-09 11:06:28 +0200

    import bootstrap et fontAwesome

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap'
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).use(store).use(router).mount('#app')
+library.add(fas);
+createApp(App).use(store).use(router).use(bootstrap).mount('#app')
