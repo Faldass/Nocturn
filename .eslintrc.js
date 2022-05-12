d6ba7ab1e9565765f3464155ee86c600c3698716
Faldass <quentin.terzi@otlook.fr>
2022-05-12 10:56:17 +0200

    Ajout composant membre et views membres

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // "no-mixed-spaces-and-tabs": 0, // disable rule
+    "no-mixed-spaces-and-tabs": 0, // disable rule
   }
 }
--- src/views/Membres.vue
+++ src/views/Membres.vue
@@ -1,10 +1,26 @@
 <template>
     <h1>Equipe</h1>
+    <div class="rowM">
+        <membreA/>
+        <membreA/>
+        <membreA/>
+    </div>
+        <membreA/>
+        <membreA/>
+        <membreA/>
 </template>
 <script>
+    import membreA from '@/components/Membre.vue'
     export default {
         name: 'membresA',
         components: {
+            membreA,
         }
     }
 </script>
+
+<style scoped>
+    .rowM{
+        display: flex;
+    }
+</style>
