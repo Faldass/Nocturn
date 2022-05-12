abddc6116affb6d6b2013a85f44112c292906695
Faldass <quentin.terzi@otlook.fr>
2022-05-12 12:27:09 +0200

    Ajout composant event et view events

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,10 +1,15 @@
 <template>
     <h1>Evenements</h1>
+    <eventA/>
+    <eventA/>
+    <eventA/>
 </template>
 <script>
+    import eventA from '@/components/Event.vue'
     export default {
         name: 'eventsA',
         components: {
-        }
+            eventA,
+        },
     }
 </script>
